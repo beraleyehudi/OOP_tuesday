851cbb0021111dd3dd9ecd8bf74d9abddc4f9ee3
berale <b.y.berale@gmail.com>
2025-06-25 09:54:17 +0300

    Hexagon class

--- shapes/hexagon.py
+++ shapes/hexagon.py
@@ -2,3 +2,10 @@ from shapes.shape import Shape
 
 
 class Hexagon(Shape):
+
+    def __init__(self, side):
+        self.side = side
+
+    def get_area(self):
+        return (3 * 3 ** 0.5 * self.side ** 2) / 2
+
